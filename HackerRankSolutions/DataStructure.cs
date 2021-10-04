440e9d5777b1afbf6e57f1a36d2ff39bb6e007b8
Shada Tosin <89518803+TosinShada@users.noreply.github.com>
2021-10-04 18:40:36 +0100

    Add project files.

---
+++ HackerRankSolutions/DataStructure.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace HackerRankSolutions
+{
+    public static class DataStructure
+    {
+        public static List<int> dynamicArray(int n, List<List<int>> queries)
+        {
+            List<List<int>> arr = new List<List<int>>();
+            int lastAnswer = 0;
+            List<int> lastAnswers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                arr.Add(new List<int>());
+            }
+
+            foreach (var query in queries)
+            {
+                var idx = (query[1] ^ lastAnswer) % n;
+                switch (query.First())
+                {
+                    case 1:
+                        arr[idx].Add(query.Last());
+                        break;
+                    case 2:
+                        lastAnswer = arr[idx][query.Last() % arr[idx].Count];
+                        lastAnswers.Add(lastAnswer);
+                        break;
+                }
+            }
+            return lastAnswers;
+        }
+
+        public static List<int> rotateLeft(int d, List<int> arr)
+        {
+            for (int i = 0; i < d; i++)
+            {
+                int temp = arr.First();
+                arr.RemoveAt(0);
+                arr.Add(temp);
+            }
+            return arr;
+        }
+    }
+}
---
+++ HackerRankSolutions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HackerRankSolutions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> array = new List<int>()
+            {
+                1,2,3,4,5
+            };
+            var result = DataStructure.rotateLeft(5, array);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
