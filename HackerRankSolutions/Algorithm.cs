3e0e9d31560137a585fc023c1bbe40d97109a47f
Shada Tosin <89518803+TosinShada@users.noreply.github.com>
2021-10-07 14:26:44 +0100

    Addition

---
+++ HackerRankSolutions/Algorithm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public static class Algorithm
+    {
+        public static List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+        {
+            List<int> sortedList = ranked.Distinct().ToList();
+            List<int> ranks = new List<int>();
+
+            for (int i = 0; i < player.Count; i++)
+            {
+                int score = player[i];
+                if (score > sortedList[0])
+                {
+                    ranks.Add(1);
+                }
+                else if (score < sortedList[sortedList.Count - 1])
+                {
+                    ranks.Add(sortedList.Count + 1);
+                }
+                else
+                {
+                    int index = BinarySearch(sortedList, score);
+                    ranks.Add(index + 1);
+
+                }
+            }
+            return ranks;
+        }
+
+        public static int BinarySearch(List<int> a, int score)
+        {
+            int min = 0;
+            int max = a.Count - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (score == a[mid])
+                {
+                    return mid;
+                }
+                else if (a[mid] < score && score < a[mid - 1])
+                {
+                    return mid;
+                }
+                else if (a[mid] > score && score >= a[mid + 1])
+                {
+                    return mid + 1;
+                }
+                else if (a[mid] < score)
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ HackerRankSolutions/WarmUp.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public static class WarmUp
+    {
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int firstSum = 0;
+            int secondSum = 0;
+
+            for (int i = 0; i < arr.Count; i++)
+            {                 
+                firstSum += arr[i][i];
+                secondSum += arr[i][arr.Count - 1 - i];
+            }
+            return Math.Abs(firstSum - secondSum);
+        }
+
+        public static void plusMinus(List<int> arr)
+        {
+            int positiveSum = 0;
+            int zeroSum = 0;
+            int negativeSum = 0;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                if (arr[i] > 0)
+                {
+                    positiveSum++;
+                }
+                else if (arr[i] == 0)
+                {
+                    zeroSum++;
+                }
+                else
+                {
+                    negativeSum++;
+                }
+            }
+            double postiveDecimal = (double)positiveSum / arr.Count;
+            double zeroDecimal = (double)zeroSum / arr.Count;
+            double negativeDecimal = (double)negativeSum / arr.Count;
+            Console.WriteLine(postiveDecimal);
+            Console.WriteLine(negativeDecimal);
+            Console.WriteLine(zeroDecimal);
+        }
+
+        public static List<int> compareTriplets(List<int> a, List<int> b)
+        {
+            int aScore = 0;
+            int bScore = 0;
+            List<int> comparedList = new List<int>();
+
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[i] > b[i])
+                {
+                    aScore++;
+                }
+                else if (a[i] < b[i])
+                {
+                    bScore++;
+                }
+            }
+            comparedList.Add(aScore);
+            comparedList.Add(bScore);
+            return comparedList;
+        }
+    }
+}
